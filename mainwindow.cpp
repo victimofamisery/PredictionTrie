8048193326e46f2e1caa77759167581d0157c9e3
victimofamisery <55550234+victimofamisery@users.noreply.github.com>
2022-06-14 05:04:39 +0300

    initial

---
+++ mainwindow.cpp
@@ -0,0 +1,42 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    std::string line;
+    std::ifstream file;
+    file.open("..\\PredictionTrie\\words.txt");
+    if (!file.is_open()) {
+        throw "file is not opened";
+    }
+    while (getline(file, line)) {
+        std::stringstream lineStream(line);
+        std::string tmpString;
+        while (getline(lineStream, tmpString, ' ')) {
+            if (!words->isPresented(tmpString)) {
+                words->insert(tmpString);
+            }
+        }
+    }
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_lineEdit_textEdited(const QString &arg1)
+{
+    QStringList complete;
+    auto vectorMatch = words->findBestMatches(arg1.toStdString(), 5);
+    for (auto word : vectorMatch) {
+        complete << QString::fromStdString(word);
+    }
+    QCompleter* completer = new QCompleter(complete);
+    ui->lineEdit->setCompleter(completer);
+}
+
